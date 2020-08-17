ba9f34fe933955e00457d49378d8b8fa186c67e7
sblackas <vivianne.traore@gmail.com>
2020-08-17 12:05:15 +0200

    invitation

--- api.js
+++ api.js
@@ -5,7 +5,32 @@ const api = express();
 api.use(express.urlencoded({extended: true}));
 api.use(express.json());
 
-// Faire la collection des students
+//Requete POST : pour ajouter des elements
+
+async function addStudent(element) {
+    let client;
+    try {
+        client = await MongoClient.connect("mongodb://localhost:27017", {useUnifiedTopology: true});
+        let db = client.db("TechWatchAssignator");
+        await db.collection("Students").insertOne(element); // On a inseré un document en l'occurence "tom" dans postman avec key=name value=tom
+    } catch (error) {
+        console.log("Oops, something went wrong, here are the details:");
+        console.log(error);
+    } finally {
+        client.close();
+    }
+}
+
+api.post("/students", function(req, res) {
+    let newStudent = {
+        name: req.body.name 
+    }
+
+    addStudent(newStudent);
+    res.send(); // Ici on veut ajouter le nom d'un nouvel élève sans pour autant envoyer qqchose d'où le res.send()
+})
+
+
 // Requete GET : pour afficher les donnees
 
 async function getStudents() { 
@@ -27,3 +52,5 @@ api.get("/students", async function(req, res) {
     let students = await getStudents();
     res.json(students); // Ici on veut envoyer nos données dans students qui fait appel à la fonction getStudents(), on met await async car cette fonction est elle même asynchrone et il faut attendre qu'elle s'execute.
 })
+
+api.listen(8000);
